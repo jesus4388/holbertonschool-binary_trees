4d25777da7909cec59e4de86f61c4c7054d69ae8
jesus4388 <jesushernandez1843@gmail.com>
2022-07-18 13:42:46 -0300

    leaf

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_leaves
+ * @tree: pointer to node
+ * Return: 0 or null
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	int a = 0;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	a = binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
+
+	return (a);
+}
