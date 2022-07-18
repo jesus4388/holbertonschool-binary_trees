e990091db45023099c04524e25e92e306ee394ba
jesus4388 <jesushernandez1843@gmail.com>
2022-07-18 11:04:10 -0300

    height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
-/*
+/**
  * binary_tree_height - size binary of tree
  * @tree: pointer
- * Return: 1 or 0 
+ * Return: 1 or 0
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
