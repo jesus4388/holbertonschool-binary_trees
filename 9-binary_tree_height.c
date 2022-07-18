b9ae7d9827acef60aea6b18f42376d304271a3ff
jesus4388 <jesushernandez1843@gmail.com>
2022-07-18 11:21:19 -0300

    height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,5 +10,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 	if (tree->left == NULL && tree->right == NULL)
 		return (0);
-	return (1 + binary_tree_height(tree->left));
+	int i = 1 + binary_tree_height(tree->left);
+	int b = 1 + binary_tree_height(tree->right);
+	
+	if (b < i)
+		return(i);
+	return (b);
 }
