9deaaba62451ec37104f44818ac6e18526e4336f
jesus4388 <jesushernandez1843@gmail.com>
2022-07-18 20:15:56 -0300

    l

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -13,6 +13,6 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (tree->left == NULL && tree->right == NULL)
 		return (0);
 	a = 1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right);
-
+	
 	return (a);
 }
