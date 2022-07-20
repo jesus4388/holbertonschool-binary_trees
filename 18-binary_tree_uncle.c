0dd696f52d8a0593ac8bcaf01d37893092603d60
jesus4388 <jesushernandez1843@gmail.com>
2022-07-20 10:57:44 -0300

    uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,7 +8,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (node == NULL)
 		return (NULL);
-	if (node->parent->parent)
+	if (node->parent && node->parent->parent)
 	{
 		if (node->parent == node->parent->parent->right)
 		{
