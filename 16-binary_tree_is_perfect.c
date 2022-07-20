4f5e896115fcb7cca8f1c6c6d0cf1352fe5e078d
jesus4388 <jesushernandez1843@gmail.com>
2022-07-20 10:18:06 -0300

    perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -32,6 +32,22 @@ size_t binary_tree_heig(const binary_tree_t *tree)
 	return (0);
 }
 /**
+ * binary_tree_size - measures size of a binary tree
+ * @tree: pointer to node
+ * Return: 0 or size
+ */
+size_t binary_size(const binary_tree_t *tree)
+{
+	int a = 0;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	a = 1 + binary_size(tree->left) +  binary_size(tree->right);
+	return (a);
+}
+/**
  * binary_tree_is_perfect - measures the balance factor
  * @tree: pointer to root
  * Return: balance of node
@@ -44,7 +60,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
-	b = binary_tree_size(tree);
+	b = binary_size(tree);
 	a = binary_tree_heig(tree);
 	if (b == a)
 		return (1);
